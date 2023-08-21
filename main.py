17c68321c1aa9d466ccd241f77ef3916499c6caa
Kirill Svidunovich <Altair21817@gmail.com>
2023-08-21 17:41:20 +0300

    Add calculate endpoints

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI, status
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 
 app: FastAPI = FastAPI()
 
 
-@app.get("/")
+class CalculateData(BaseModel):
+    num1: int
+    num2: int
+
+
+@app.get('/')
 async def index():
     with open(file='index.html',
               mode='r',
               encoding='utf-8') as html_file:
         html_content = html_file.read()
     return HTMLResponse(content=html_content, status_code=status.HTTP_200_OK)
+
+
+@app.post('/calculate_path/')
+async def calculate_path(num1: int, num2: int):
+    return {"result": num1+num2}
+
+
+@app.post('/calculate_body/')
+async def calculate_body(data: CalculateData):
+    return {"result": data.num1 + data.num2}
