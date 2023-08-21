8a7244a219f992d7c51327cea43a86b64dcbdb3a
Kirill Svidunovich <Altair21817@gmail.com>
2023-08-21 17:53:26 +0300

    Add semi-start for uvicorn

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+import uvicorn
+
 from fastapi import FastAPI, status
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 
-app: FastAPI = FastAPI()
+app: FastAPI = FastAPI(title='My first FastAPI app')
 
 
 class CalculateData(BaseModel):
@@ -27,3 +29,6 @@ async def calculate_path(num1: int, num2: int):
 @app.post('/calculate_body/')
 async def calculate_body(data: CalculateData):
     return {"result": data.num1 + data.num2}
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
