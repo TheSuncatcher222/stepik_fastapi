89337ca911c445cfe222661eff2f36ac240d800a
Kirill Svidunovich <Altair21817@gmail.com>
2023-08-21 19:10:32 +0300

    Add users_get

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
 import uvicorn
-
 from fastapi import FastAPI, status
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+
+from core.core import check_age_grade
+from models.models import CalculateData, Users
 
 app: FastAPI = FastAPI(title='My first FastAPI app')
-
-
-class CalculateData(BaseModel):
-    num1: int
-    num2: int
 
 
 @app.get('/')
@@ -29,6 +25,19 @@ async def calculate_path(num1: int, num2: int):
 @app.post('/calculate_body/')
 async def calculate_body(data: CalculateData):
     return {"result": data.num1 + data.num2}
+
+
+@app.post('/users/')
+async def users_get(user: Users):
+    posted_user: Users = Users(id=user.id,
+                               name=user.name,
+                               age=user.age)
+    return {'id': posted_user.id,
+            'name': posted_user.name,
+            'age': posted_user.age,
+            'date_reg': posted_user.date_reg,
+            'status': check_age_grade(age=posted_user.age)}
+
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
