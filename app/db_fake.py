e95c01b923e12dda21fc2d7c284d6b3e9b1a76ac
Kirill Svidunovich <Altair21817@gmail.com>
2023-08-22 16:16:37 +0300

    Delete unused imports

--- app/db_fake.py
+++ app/db_fake.py
@@ -1,6 +1,5 @@
-from core.core import PRODUCTS, USERS, check_age_grade
-from models.models import UsersAgeGrade
+from core.core import PRODUCTS, USERS
 
 DB_FAKE_INIT: dict[str, dict[int, any]] = {
     PRODUCTS: {},
-    USERS: {},}
+    USERS: {}}
