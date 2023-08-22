d8992ea728155f096b0624031aec81be0ef2f5cf
Kirill Svidunovich <Altair21817@gmail.com>
2023-08-22 16:11:20 +0300

    Update to DB_FAKE_INIT

--- app/db_fake.py
+++ app/db_fake.py
@@ -1,1 +1,6 @@
-db_fake: list = []
+from core.core import PRODUCTS, USERS, check_age_grade
+from models.models import UsersAgeGrade
+
+DB_FAKE_INIT: dict[str, dict[int, any]] = {
+    PRODUCTS: {},
+    USERS: {},}
