ca6abcdcb93f74df8d57df9b2970c35d9b117b35
Kirill Svidunovich <Altair21817@gmail.com>
2023-08-22 16:11:06 +0300

    Add products and users names

--- app/core/core.py
+++ app/core/core.py
@@ -1,9 +1,13 @@
+ADULT_AGE: int = 18
 ADULT: str = 'adult'
 MINOR: str = 'minor'
+
+PRODUCTS: str = 'products'
+USERS: str = 'users'
 
 
 def check_age_grade(age: int) -> str:
     """Return age grade:
         - minor for age under 18;
         - adult in other case."""
-    return ADULT if age >= 18 else MINOR
+    return ADULT if age >= ADULT_AGE else MINOR
