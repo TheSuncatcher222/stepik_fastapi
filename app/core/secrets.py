aea8974bc427dabe98653728978e1b732d2d27a1
Kirill Svidunovich <Altair21817@gmail.com>
2023-08-28 17:23:49 +0300

    Collect secrets to load from dotenv in future

---
+++ app/core/secrets.py
@@ -0,0 +1,9 @@
+"""Auth hash parameters"""
+HASH_NAME = 'sha256'
+PASS_ENCODE = 'utf-8'
+SALT = b'\x0eE\x1f\x7f\x92\xe3\x19\xbdzz\xae"\x19\xca\x07E'
+ITERATIONS = 1000
+
+"""Encryption data."""
+SECRET_KEY = b'some_secret_key_here'
+SECRET_TOKEN: str = 'secrets_token'
