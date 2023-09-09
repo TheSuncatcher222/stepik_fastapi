a12334698ef786dc88dcbda7d9d89c4fc8a2ebb8
Kirill Svidunovich <Altair21817@gmail.com>
2023-09-09 10:39:01 +0300

    Load secrets from .env

--- app/core/secrets.py
+++ app/core/secrets.py
@@ -1,14 +1,19 @@
 """
 Load secrets from .env file
 """
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 """Auth hash parameters"""
-HASH_NAME = 'sha256'
-PASS_ENCODE = 'utf-8'
-SALT = b'\x0eE\x1f\x7f\x92\xe3\x19\xbdzz\xae"\x19\xca\x07E'
-ITERATIONS = 1000
+HASH_NAME = os.getenv('HASH_NAME')
+PASS_ENCODE = os.getenv('PASS_ENCODE')
+SALT = bytes(os.getenv('SALT'), encoding=PASS_ENCODE)
+ITERATIONS = int(os.getenv('ITERATIONS'))
 
 """Encryption data."""
-JWT_ALGORITHM = 'HS256'
-JWT_EXPIRATION_SEC = 10
-SECRET_KEY = b'some_secret_key_here'
+JWT_ALGORITHM = os.getenv('JWT_ALGORITHM')
+JWT_EXPIRATION_SEC = int(os.getenv('JWT_EXPIRATION_SEC'))
+SECRET_KEY = os.getenv('SECRET_KEY')
